0a3f60bcaa18a226becc6f5f55ed486f62e3225f
Jody Unterschutz <jody.u01@gmail.com>
2020-02-15 12:55:57 -0700

    Getting DB ready

---
+++ Data/JUGContext.cs
@@ -0,0 +1,18 @@
+﻿using Data.DAO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class JUGContext:DbContext
+    {
+        public DbSet<TeamDAO> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlite("Data Source=D:/data/teamtest.db");
+        }
+    }
+}
---
+++ Data/Migrations/20200215195211_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Skill = table.Column<int>(nullable: false),
+                    TeamType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}

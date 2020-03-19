5f40e599f2ec6202cc6bb0d00778e84da14daacd
jodyu <jody_unterschutz@tcenergy.com>
2020-03-19 10:35:57 -0600

    next

---
+++ BlazorWeb/Services/ViewModels/Teams/ITeamViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Services.ViewModels.Teams
+{
+    public class TeamViewModel
+    {                
+        public TeamType TeamType { get; set; }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Skill { get; set; }
+    }
+
+    public enum TeamType
+    {        
+        BaseTeam = 0,        
+        SeasonTeam = 1
+    }
+
+}

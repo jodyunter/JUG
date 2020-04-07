c00f2ed17a41fb8b99895b313a9dcd266ccf25f3
jodyu <jody_unterschutz@tcenergy.com>
2020-04-07 15:27:42 -0600

    done

--- BlazorWeb/Shared/AppState.cs
+++ BlazorWeb/Shared/AppState.cs
@@ -8,9 +8,14 @@ namespace BlazorWeb
     public class AppState
     {
         public object EditModel { get; set; }
+        public object[] ListData { get; set; }
 
         public event Action ModelSelected;
+        public event Action ListDataChanged;
         public event Func<Task<bool>> ModelReadyToSave;
+        
+        public event Func<long, Task<bool>> ModelSelectedToEdit;
+        public event Func<long, Task<bool>> ModelSelectedToDelete;
 
         public void SetModelForEdit(object model)
         {
@@ -24,7 +29,27 @@ namespace BlazorWeb
             NotifyModelReadyForUpdate();
         }
 
+        public void ChooseModelForEdit(long id)
+        {
+            NotifyModelChosenForEdit(id);
+        }
+
+        public void ChooseModelForDelete(long id)
+        {
+            NotifyModelChosenForDelete(id);
+        }
+
+        public void SetListData(object[] data)
+        {
+            ListData = data;            
+
+            NotifyListDataChanged();
+        }
+
         private void NotifyModelSelected() => ModelSelected?.Invoke();
         private void NotifyModelReadyForUpdate() => ModelReadyToSave?.Invoke();
+        private void NotifyModelChosenForEdit(long id) => ModelSelectedToEdit?.Invoke(id);
+        private void NotifyModelChosenForDelete(long id) => ModelSelectedToDelete?.Invoke(id);
+        private void NotifyListDataChanged() => ListDataChanged?.Invoke();
     }
 }

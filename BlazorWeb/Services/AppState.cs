9cad1c2a76e93bc48ed60bf97d0398bd87e26d22
jodyu <jody_unterschutz@tcenergy.com>
2020-04-07 14:04:59 -0600

    m

---
+++ BlazorWeb/Services/AppState.cs
@@ -0,0 +1,13 @@
+﻿using Services.ViewModels.Teams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AppState
+    {
+        public Object EditModel { get; set; } = new TeamViewModel() { Id = 0, Name = "Edit Me", Skill = 15, TeamType = TeamType.BaseTeam };
+    }
+}
